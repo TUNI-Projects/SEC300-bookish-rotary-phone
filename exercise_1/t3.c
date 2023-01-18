8fbd304dd1e9a44c95340cecdaf9ea12162901ac
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-01-18 10:31:40 +0200

    t3 added

--- exercise_1/t3.c
+++ exercise_1/t3.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * Write a program that causes heap overflow
  */
 
-int main()
+int main(int argc, char *argv[])
 {
+    int size = atoi(argv[1]);
+    // printf("%d\n", size);
+    char *buffer = (char *)malloc(size);
+    // printf("%s\n", argv[2]);
+    strcpy(buffer, argv[2]);
+    // printf("%s\n", buffer);
     return 0;
 }
